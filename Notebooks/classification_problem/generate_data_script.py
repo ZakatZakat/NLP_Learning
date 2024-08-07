5b216cc467cc09133e388f1fc2f420b01b16d633
Zakat_Zakat <you@example.com>
2024-08-07 16:32:46 +0300

    update

--- Notebooks/classification_problem/generate_data_script.py
+++ Notebooks/classification_problem/generate_data_script.py
@@ -143,5 +143,5 @@ for epoch in range(2):
         correct += (predicted == labels).sum().item()
 
     accuracy = correct / total
-    print(f'epoch: {epoch}, accuracy: {accuracy})
+    print(f'epoch: {epoch}, accuracy: {accuracy}')
 
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## Results
 ```
-
+epoch: 0, accuracy: 0.19683377308707123
+epoch: 1, accuracy: 0.2554089709762533
 ```
 More details about the project. UPD
