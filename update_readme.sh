bfa263fc8be10255712c7f12849581ea11e7e6ef
Zakat_Zakat <you@example.com>
2024-08-07 12:07:23 +0300

    update_workflow

--- update_readme.sh
+++ update_readme.sh
@@ -13,4 +13,4 @@ echo "$OUTPUT" >> README.md
 echo "\`\`\`" >> README.md
 
 # Optionally, add more static or dynamic content to the README
-echo "More details about the project." >> README.md
+echo "More details about the project. UPD" >> README.md
